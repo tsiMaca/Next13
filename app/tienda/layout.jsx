17c917a4fe0924bc28b95a8e0cfadd5d789c1219
tsiMaca <santos.macarenat@gmail.com>
2023-09-11 16:32:06 -0700

    tiendaLayout added

--- app/tienda/layout.jsx
+++ app/tienda/layout.jsx
@@ -1,12 +1,18 @@
+import Link from "next/link"
 export default function TiendaLayout({children}) {
 
     return <>
     <navbar>
     <h3>Seccion tienda</h3>
     <ul>
-        <li>Categorias</li>
-        <li>Productos</li>
-    </ul>
-    </navbar>
+          <li>
+            <Link href="/tienda/categorias">Categorias </Link>
+          </li>
+          <li>
+          <Link href="/tienda/categorias/computadoras"> Computadoras </Link>
+          </li>
+         
+        </ul>
+       </navbar>
     {children}</> 
 }
